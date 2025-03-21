07faca1ca07cd6a8e1b23e7dabb9427919ce4e23
SerraSilvia <152980740+SerraSilvia@users.noreply.github.com>
2025-03-21 12:07:29 +0000

    add noticiesCultura

---
+++ veureNoticiesCultura.php
@@ -0,0 +1,17 @@
+<?php
+
+$db = new SQLite3('diariLocal.db');
+
+$resultats = $db->query("SELECT not_id, not_titular, not_cos, not_data, not_seccio FROM noticies WHERE LOWER(not_seccio) = 'cultura'");
+
+while ($fila = $resultats->fetchArray(SQLITE3_ASSOC)){
+    echo "ID: ". $fila['not_id'] . "<br>".  
+    " - TITULAR: " . $fila['not_titular'] . "<br>" .
+    " - COS: " . $fila['not_cos']  . "<br>" .
+    " - DATA: " . $fila['not_data']  . "<br>" .
+    " - SECCIO: " . $fila['not_seccio']  . "<br>" .
+    "<br><br>";
+}
+
+$db -> close();
+?>
