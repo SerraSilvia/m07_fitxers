b5e317f2480c83a2a8b150ac0338144f4c1f9997
SerraSilvia <152980740+SerraSilvia@users.noreply.github.com>
2025-03-21 11:52:21 +0000

    fix veure totes noticies

--- veureTotesLesNoticies.php
+++ veureTotesLesNoticies.php
@@ -1,18 +1,16 @@
 <?php
-echo "hola";
 
-$db = new SQLite('diariLocal.db');
+$db = new SQLite3('diariLocal.db');
 
 $resultats = $db->query("SELECT * FROM noticies");
 
 while ($fila = $resultats->fetchArray(SQLITE3_ASSOC)){
-    echo "dins";
-    /*echo "ID: ". $fila['not_id'] .
-    " - TITULAR: ". $fila['not_titular'] .
-    " - COS: ". $fila['not_cos'] .
-    " - DATA: ". $fila['not_data'] .
-    " - SECCIO: ". $fila['not_seccio'] .
-    "<br>";*/
+    echo "ID: ". $fila['not_id'] . "<br>".  
+    " - TITULAR: " . $fila['not_titular'] . "<br>" .
+    " - COS: " . $fila['not_cos']  . "<br>" .
+    " - DATA: " . $fila['not_data']  . "<br>" .
+    " - SECCIO: " . $fila['not_seccio']  . "<br>" .
+    "<br><br>";
 }
 
 
